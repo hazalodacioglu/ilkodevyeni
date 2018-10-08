742dad2127b10be032a8c99aecf462ccd3f3f5ce
hazalodacioglu <43784609+hazalodacioglu@users.noreply.github.com>
2018-10-08 15:25:30 +0300

    Add files via upload

---
+++ Form4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form4 : Form
+    {
+        Bitmap kaynak, islem;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void rgbToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    Color siraliRenk = Color.FromArgb(renkliRenk.B, renkliRenk.R, renkliRenk.G);
+                    islem.SetPixel(x, y, siraliRenk);
+                }
+            }
+
+            islemBox.Image = islem;
+        }
+
+        private void rgbgbrToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    Color siraliRenk = Color.FromArgb(renkliRenk.G, renkliRenk.B, renkliRenk.R);
+                    islem.SetPixel(x, y, siraliRenk);
+                }
+            }
+
+            islemBox.Image = islem;
+        }
+
+        private void açToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            DialogResult sonuc = openFileDialog1.ShowDialog();
+            if (sonuc == DialogResult.OK)
+            {
+                kaynak = new Bitmap(openFileDialog1.FileName);
+                kaynakBox.Image = kaynak;
+            }
+        }
+    }
+}
